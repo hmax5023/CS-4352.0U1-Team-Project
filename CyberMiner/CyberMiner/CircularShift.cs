cfdc3e07daafd74ea70ff89733e9be2c71f306d2
hmax5023 <32879897+hmax5023@users.noreply.github.com>
2018-06-13 20:21:09 -0500

    2 line update in circularshift.cs

---
+++ CyberMiner/CyberMiner/CircularShift.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberMiner
+{
+    //creates virtual lines of the circular shifts of stored lines
+    //provides routines to access individual characters and words of shifted lines
+    public class CircularShift
+    {
+        //constructor
+        public LineStorage LineStorage { get; set; }
+        public CircularShift(LineStorage LS)
+        {
+            LineStorage = LS;
+        }
+
+        public void Setup()
+        {
+            char cr = LineStorage.getChar();
+            int wd = LineStorage.getWord();
+        }
+        //causes the character in a word of the shift to be NextChar
+        private void SetChar(int Shift, int Word, int Character, char NextChar)
+        {
+
+        }
+
+        public string Char(int Shift, int Word, string NextChar)
+        {
+            return NextChar; //placeholder
+        }
+
+        //returns number of words in shift
+        public int Word(int Shift)
+        {
+            return Shift; //placeholder
+        }
+    }
+}
